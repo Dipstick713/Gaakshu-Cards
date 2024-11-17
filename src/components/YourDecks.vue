23542b8a01a971c50ab902b3ed6f17d59051087f
Dipstick713 <139313088+Dipstick713@users.noreply.github.com>
2024-11-18 00:55:35 +0530

    Update YourDecks.vue

--- src/components/YourDecks.vue
+++ src/components/YourDecks.vue
@@ -1,10 +1,11 @@
 <script setup>
-import { RouterLink } from 'vue-router';
+import { RouterLink, useRouter } from 'vue-router';
 import { ref, computed, onMounted } from 'vue';
 import { supabase } from '@/supabase';
 import store from '@/store';
 const user = computed(()=> store.state.user);
 const decks=ref([]);
+const router = useRouter();
 if(user)
   {onMounted(async () => {
     const { data: { user }, error: userError } = await supabase.auth.getUser()
@@ -17,15 +18,19 @@ if(user)
 });}
 const displayedDecks = computed(() => decks.value.slice(0, 5));
 const hasMoreDecks = computed(() => decks.value.length > 4);
+const handleDeckSelect = (deckId) => {
+  router.push(`/Study/${deckId}`);
+};
+
 </script>
 
 <template>
   <div class="bg-gray-900 text-white">
     <h2 v-if="decks.length > 0" class="text-2xl font-bold mb-4">Your Decks</h2>
     <div class="flex flex-wrap ">
-      <div v-for="deck in displayedDecks" :key="deck.id" class="w-64 h-40 m-2 bg-gray-800 rounded-lg p-4 flex flex-col items-center justify-center hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 focus:ring-offset-gray-900 transition-colors duration-300">
+      <button v-for="deck in displayedDecks" :key="deck.id" @click="handleDeckSelect(deck.id)" class="w-64 h-40 m-2 bg-gray-800 rounded-lg p-4 flex flex-col items-center justify-center hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 focus:ring-offset-gray-900 transition-colors duration-300">
         <h3 class="text-lg font-semibold">{{ deck.deck_name }}</h3>
-      </div>
+      </button>
     </div>
     <div v-if="hasMoreDecks" class="flex justify-end mt-4">
       <RouterLink to="/Decks">
